6bcf962cbab1efbc62bf4a63a3c2841fc416fd1c
THC-Lab <72765359+TheGreatSkeeve@users.noreply.github.com>
2022-08-04 20:26:49 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -78,19 +78,11 @@ def bofh():
 def texts():
     print(request.headers)
     if request.method == 'POST':
-        try:
-            with open('file.txt','w') as x:
-                x.write(request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr))
-                x.close()
-            if '216.36.27.41' in request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr):
-                content = request.headers.get('content')
-                emailSender(content)
-            else:
-                url = "https://api.telegram.org/bot5585546662:AAG4_54V68C4howzaqkVwsRTW5WAQeYAH5c/sendMessage?chat_id=-426528357&text=Unauthorized API Usage from {}".format(request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr))
-                requests.get(url)
-        except:
-            url = "https://api.telegram.org/bot5585546662:AAG4_54V68C4howzaqkVwsRTW5WAQeYAH5c/sendMessage?chat_id=-426528357&text=Unauthorized API Usage from {}".format(
-                request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr))
+        if '216.36.27.41' in request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr):
+            content = request.headers.get('content')
+            emailSender(content)
+        else:
+            url = "https://api.telegram.org/bot5585546662:AAG4_54V68C4howzaqkVwsRTW5WAQeYAH5c/sendMessage?chat_id=-426528357&text=Unauthorized API Usage from {}".format(request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr))
             requests.get(url)
 
 # Password generator
