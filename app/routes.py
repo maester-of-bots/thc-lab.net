e0917fc184e8b99bd9a81672db3488d830e7b8ba
THC-Lab <72765359+TheGreatSkeeve@users.noreply.github.com>
2022-08-24 19:44:19 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -56,7 +56,7 @@ def captain():
 def fuckingip():
     # ip_addr = request.environ['REMOTE_ADDR']
     data = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    loc_data = geocoder.ip(data)
+    loc_data = geocoder.ip(data).json
     return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=loc_data['hostname'],lat=loc_data['lat'],lon=loc_data['lon'],isp=loc_data['org'],zipcode=loc_data['postal'])
 
 
