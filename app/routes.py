0b929d45f27d239aa9e66fd535f2956dc289d190
THC-Lab <72765359+TheGreatSkeeve@users.noreply.github.com>
2022-08-24 20:51:22 -0400

    d

--- app/routes.py
+++ app/routes.py
@@ -67,7 +67,7 @@ def fuckingip():
         hostname = loc_data['hostname']
     else:
         hostname = ''
-    return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],zipcode=loc_data['postal'])
+    return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url=loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
 
 
 # Bad Ape
