1761bc6aa2e15e2b955ffaaa0701f7c64cd57bf1
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2022-11-25 16:38:54 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -12,13 +12,14 @@ import os
 
 from hashids import Hashids
 
+
 def emailSender(content):
     to = "5714779283@tmomail.net"
     sender = "steven.haering@gmail.com"
     key = "wilkbcaineyzjein"
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login(sender, key)
-    server.sendmail(sender,to,content)
+    server.sendmail(sender, to, content)
 
 
 '''
@@ -43,13 +44,16 @@ hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 # Main page, there's nothing here...
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',  small_title="Steve's Random Python Website",big_title="Steve's Random Python Website", description="Description",image_url="image.jpg")
+    return render_template('index.html', small_title="Steve's Random Python Website",
+                           big_title="Steve's Random Python Website", description="Description", image_url="image.jpg")
 
 
 # Captain website
 @app.route('/captain.html')
 def captain():
-    return render_template('captain.html', small_title="BITCHES AND HOES", description="Captain Memorial Website",image_url="pm.png")
+    return render_template('captain.html', small_title="BITCHES AND HOES", description="Captain Memorial Website",
+                           image_url="pm.png")
+
 
 # Captain website
 @app.route('/status.html')
@@ -62,7 +66,7 @@ def status():
 def vizzyt():
     westeros = os.listdir('app/static/img/FFS/MiddleEarth')
     middle_earth = os.listdir('app/static/img/FFS/Westeros')
-    return render_template('vizzyt.html', pics=(westeros+middle_earth))
+    return render_template('vizzyt.html', pics=(westeros + middle_earth))
 
 
 # Captain website
@@ -75,7 +79,9 @@ def fuckingip():
         hostname = loc_data['hostname']
     else:
         hostname = ''
-    return render_template('fuckingip.html', ip=loc_data['ip'],address=loc_data['address'],hostname=hostname,lat=loc_data['lat'],lon=loc_data['lng'],isp=loc_data['org'],isp_url="https://ipinfo.io/"+loc_data['org'].split(" ")[0],zipcode=loc_data['postal'])
+    return render_template('fuckingip.html', ip=loc_data['ip'], address=loc_data['address'], hostname=hostname,
+                           lat=loc_data['lat'], lon=loc_data['lng'], isp=loc_data['org'],
+                           isp_url="https://ipinfo.io/" + loc_data['org'].split(" ")[0], zipcode=loc_data['postal'])
 
 
 # Bad Ape
@@ -88,17 +94,19 @@ def yugioh():
                 data = card.getInfo()
                 return render_template('yugioh.html', small_title='YuGiOh Card Searcher', response=data)
     else:
-        return render_template('yugioh.html', small_title="YuGiOh Card Searcher", title='YuGiOh Card Searcher', description="Description",image_url="image.jpg")
+        return render_template('yugioh.html', small_title="YuGiOh Card Searcher", title='YuGiOh Card Searcher',
+                               description="Description", image_url="image.jpg")
 
 
 # BOFH Simulator
 @app.route('/bofh.html')
 def bofh():
-    x = random.randint(0,465)
-    response=bofh_text[x].capitalize()
-    lmgtfy="https://www.google.com/search?q=" + response.replace(" ","+")
+    x = random.randint(0, 465)
+    response = bofh_text[x].capitalize()
+    lmgtfy = "https://www.google.com/search?q=" + response.replace(" ", "+")
 
-    return render_template('bofh.html', small_title='IT Help Desk', response=response, link=lmgtfy, description="Description",image_url="image.jpg")
+    return render_template('bofh.html', small_title='IT Help Desk', response=response, link=lmgtfy,
+                           description="Description", image_url="image.jpg")
 
 
 # Text Messages
@@ -108,7 +116,10 @@ def texts():
     if request.method == 'POST' and '216.36.27.41' in ip:
         emailSender(request.headers.get('content'))
     else:
-        requests.get("https://api.telegram.org/bot5585546662:AAG4_54V68C4howzaqkVwsRTW5WAQeYAH5c/sendMessage?chat_id=-426528357&text=Unauthorized API Usage from {}".format(ip))
+        requests.get(
+            "https://api.telegram.org/bot5585546662:AAG4_54V68C4howzaqkVwsRTW5WAQeYAH5c/sendMessage?chat_id=-426528357&text=Unauthorized API Usage from {}".format(
+                ip))
+
 
 # Password generator
 @app.route('/pass.html', methods=['GET', 'POST'])
@@ -116,33 +127,37 @@ def passbuddy():
     if request.method == 'POST':
         result = request.form
     elif request.method == 'GET':
-        result="kids"
-    kid_count,teen_count,adult_count,swear_count,xxx_count = getCount()
+        result = "kids"
+    kid_count, teen_count, adult_count, swear_count, xxx_count = getCount()
     passwords, plaintexts = getWords(result)
     password = passwords[1] + passwords[0] + getSpecial()
     password = ''.join(addCap(password))
-    plaintext = plaintexts[1].capitalize()  +" "+ plaintexts[0].capitalize()
-    return render_template('pass.html', small_title="Steve's Awesome Password Generator", description="Description",image_url="image.jpg", password=password, plaintext=plaintext, kid_count=kid_count, teen_count=teen_count ,adult_count=adult_count, swear_count=swear_count, xxx_count=xxx_count)
+    plaintext = plaintexts[1].capitalize() + " " + plaintexts[0].capitalize()
+    return render_template('pass.html', small_title="Steve's Awesome Password Generator", description="Description",
+                           image_url="image.jpg", password=password, plaintext=plaintext, kid_count=kid_count,
+                           teen_count=teen_count, adult_count=adult_count, swear_count=swear_count, xxx_count=xxx_count)
 
 
 # Bad Ape
 @app.route('/badape.html', methods=['GET', 'POST'])
 def badape():
-    meltdownScore=""
-    meltPosts=""
-    meltComments=""
+    meltdownScore = ""
+    meltPosts = ""
+    meltComments = ""
     if request.method == 'POST':
         data = request.form.to_dict()
         meltdownScore, meltPosts, meltComments = meltdownCalc(data['Name'])
 
     print(meltdownScore, meltPosts, meltComments)
-    return render_template('badape.html', small_title='ShillScore Calculator', description="Description",image_url="image.jpg", response=meltdownScore, meltPosts=meltPosts,meltComments=meltComments)
+    return render_template('badape.html', small_title='ShillScore Calculator', description="Description",
+                           image_url="image.jpg", response=meltdownScore, meltPosts=meltPosts,
+                           meltComments=meltComments)
 
 
 # Shows various things I've worked on
 @app.route('/projects.html')
 def projects():
-    return render_template('projects.html', small_title='Projects', description="Description",image_url="image.jpg")
+    return render_template('projects.html', small_title='Projects', description="Description", image_url="image.jpg")
 
 
 @app.route('/sitemap.xml')
@@ -175,9 +190,10 @@ def shorts():
         hashid = hashids.encode(url_id)
         short_url = request.host_url + hashid
 
-        return render_template('shorts.html', small_title="URL Shortener", short_url=short_url, description="Description",image_url="image.jpg")
+        return render_template('shorts.html', small_title="URL Shortener", short_url=short_url,
+                               description="Description", image_url="image.jpg")
 
-    return render_template('shorts.html', small_title="URL Shortener", description="Description",image_url="image.jpg")
+    return render_template('shorts.html', small_title="URL Shortener", description="Description", image_url="image.jpg")
 
 
 @app.route('/404.html')
@@ -189,19 +205,22 @@ def x404():
 def x400():
     return render_template('400.html')
 
+
 @app.route('/666.html')
 def x666():
     return render_template('666.html')
+
 
 @app.route('/recs.html')
 def recs():
     return render_template('recs.html')
 
-	
+
 @app.route('/ffs/<pic>')
 def ffs_pic(pic):
-	return send_file(f'static/img/FFS/Westeros/{pic}')
-	
+    return send_file(f'static/img/FFS/Westeros/{pic}')
+
+
 @app.route('/<id>')
 def url_redirect(id):
     conn = get_db_connection()
@@ -216,14 +235,14 @@ def url_redirect(id):
         clicks = url_data['clicks']
 
         conn.execute('UPDATE urls SET clicks = ? WHERE id = ?',
-                     (clicks+1, original_id))
+                     (clicks + 1, original_id))
 
         conn.commit()
         conn.close()
         return redirect(original_url)
     else:
         flash('Invalid URL')
-        num = random.randint(0,3)
+        num = random.randint(0, 3)
         if num == 0:
             return redirect('/404.html')
         elif num == 1:
@@ -231,8 +250,6 @@ def url_redirect(id):
         elif num == 2:
             return redirect('/666.html')
         # return redirect(url_for('index'))
-
-
 
 
 # Error Handling
@@ -247,4 +264,3 @@ def hahafuckyou_1(e):
 def hahafuckyou_2(e):
     # note that we set the 404 status explicitly
     return redirect("http://thc-lab.net:8080", code=302)
-
