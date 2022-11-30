10a97c25b86b8eed8d8ad22ed3bfd66d4019f9f7
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2022-11-30 11:57:56 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -114,6 +114,8 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# x
+
 @app.route('/upload', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
