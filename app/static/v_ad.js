be216d3f21a7474be94068621a4896e79dc463a3
Maester of Bots <111409615+maester-of-bots@users.noreply.github.com>
2023-02-23 18:33:07 -0500

    ads

---
+++ app/static/v_ad.js
@@ -0,0 +1,8 @@
+(function(__htavim){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = __htavim || {};
+s.src = "\/\/nifty-transportation.com\/dVmpF.zodAGmlCtCP_3fptvbbmmcVAJOZWDf0-0\/N\/j\/QS5gNiTmQr5ULQTmQi2\/NyDtk-1iNUTbAX";
+l.parentNode.insertBefore(s, l);
+})()
