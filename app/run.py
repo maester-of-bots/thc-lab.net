908fe9bb6feaa6b98089ca94960168f9913abf17
Steve <111409615+maester-of-bots@users.noreply.github.com>
2025-08-11 19:16:02 -0400

    Update run.py

--- app/run.py
+++ app/run.py
@@ -29,7 +29,7 @@ def configure_database(app):
             # fallback to SQLite
             basedir = os.path.abspath(os.path.dirname(__file__))
             app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-
+            # Gay
             print('> Fallback to SQLite ')
             db.create_all()
 
